ab963d3077d67b0494005449892285b46ac2ad34
Eti Priya <etipriya@gmail.com>
2021-05-19 15:08:04 +0100

    declared dependencies and route to index.html

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+//Dependencies
 const express = require("express");
-const PORT = process.env.PORT || 3000;
+const path = require("path");
+const fs =require("fs");
+
+// Set the express app
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 const handleRequest = (req, res) => {
   res.send("Hello World!");
 };
-app.get("/", handleRequest);
+
+//Declaring the route that sends the user to index.html page
+app.get("/",function(req, res));{
+  res.send(path.join(__dirname, "public/index.html"))
+}
+
+app.get("/", (req, res) => {
+  res.send("Start working with Note taker");
+});
+
+//Listener
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`App listening on port: ${PORT}`);
 });

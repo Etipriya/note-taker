1247e30fb7f6103898c1fb8c3ae39c6fb730835f
Eti Priya <etipriya@gmail.com>
2021-05-19 14:47:12 +0100

    changed the name of index.js file to server.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const handleRequest = (req, res) => {
+  res.send("Hello World!");
+};
+app.get("/", handleRequest);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});

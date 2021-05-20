456ec23ead38aa57838c215c3aa66683c20689ac
Eti Priya <etipriya@gmail.com>
2021-05-20 18:40:32 +0100

    added static files

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const { dirname } = require("path");
 const app = express();
 
 //Serve static files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 const PORT = process.env.PORT || 3000;
 
@@ -32,12 +34,14 @@ app.get("/api/notes", (req, res) => {
 
 //Create new note with message to db.json file
 app.post("/api/notes", function (req, res) {
+  console.log(req.body);
   fs.readFile(path.join(__dirname, "/db/db.json"), function (error, response) {
     if (error) {
       console.log(error);
     }
     const notes = JSON.parse(response);
     const noteRequest = req.body;
+
     const newNoteId = notes.length + 1;
     const newNote = {
       id: newNoteId,
@@ -58,12 +62,37 @@ app.post("/api/notes", function (req, res) {
 
 //Delete the note with requested id from db.json file
 app.delete("/api/notes/:id", function (req, res) {
+  console.log(req.params.id);
   const deleteId = req.params.id;
-  fs.readFile("/db/db.json", "utf8", function (error, response) {
-    if (error) {
-      console.log(error);
+  fs.readFile(
+    path.join(__dirname, "/db/db.json"),
+    "utf8",
+    function (error, response) {
+      if (error) {
+        console.log(error);
+      }
+      let notes = JSON.parse(response);
+      if (deleteId <= notes.length) {
+        // Method to remove an element from an array
+        res.json(notes.splice(deleteId - 1, 1));
+        // Reassign the ids of all notes
+        for (let i = 0; i < notes.length; i++) {
+          notes[i].id = i + 1;
+        }
+        fs.writeFile(
+          path.join(__dirname, "/db/db.json"),
+          JSON.stringify(notes, null, 2),
+          function (err) {
+            if (err) throw err;
+          }
+        );
+      } else {
+        res.json(false);
+      }
     }
-    
+  );
+});
+
 //Listener
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
